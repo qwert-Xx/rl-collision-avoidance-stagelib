3ec8ea51e99d2caa482c7b46573cefd46dbf974a
qwert_X <1010973798@qq.com>
2024-11-06 19:39:08 +0800

    添加了初始化方法

---
+++ StageLibCpp/include/stgCPPToPy.hh
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <Stage-4.3/stage.hh>
+namespace StgCPPToPy {
+    extern Stg::World*  world;
+    extern std::vector<Stg::World*> worlds; //世界列表
+    const std::string stageFileStr = "stage1.world";
+    int Init(uint8_t num_world);
+
+    
+
+
+}
+
+
